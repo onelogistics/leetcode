80a0f63e66ee82a1d1b47ae528e3c761656e316e
yangjunjun <yangjunjun@bbktel.com>
2019-12-30 21:06:45 +0800

    寻找二叉树最大路径初版

---
+++ src/main/java/com/ai/algorithm/tree/FindMaxPathSum.java
@@ -0,0 +1,12 @@
+package com.ai.algorithm.tree;
+
+/**求二叉树中最大路径之和（路径之和即为节点权重的累加，权重有可能为负数）
+ *        -10
+ *       2    6
+ *   -2   -3  -4  5
+ *   则最大路径和为节点6到节点5，最终结果为11
+ * @author JunjunYang
+ * @date 2019/12/30 20:41
+ */
+public class FindMaxPathSum {
+}
