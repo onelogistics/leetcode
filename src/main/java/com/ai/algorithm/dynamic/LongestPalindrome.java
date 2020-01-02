c62776693e135333168930b7671e9dc571ea1774
yangjun <1141734656@qq.com>
2020-01-03 00:48:11 +0800

    最长回文字符串

--- src/main/java/com/ai/algorithm/dynamic/LongestPalindrome.java
+++ src/main/java/com/ai/algorithm/dynamic/LongestPalindrome.java
@@ -1,6 +1,7 @@
 package com.ai.algorithm.dynamic;
 
 /**
+ * 时间复杂度O(n*n)
  * 最长回文字符串 leetcode 5
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
  * Input: "babad"
@@ -8,4 +9,41 @@ package com.ai.algorithm.dynamic;
  * Note: "aba" is also a valid answer.
  */
 public class LongestPalindrome {
+    //起始位置
+    int lo;
+    //回文长度
+    int maxLen;
+
+    public String solution(String s) {
+        lo = 0;
+        maxLen = 0;
+        if (s.length() < 2) {
+            return s;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            extendPalindrome(s, i, i);//尝试以自身为中心扩大
+            extendPalindrome(s, i, i + 1);//尝试以i和i+1中间作为中心扩大
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    /**
+     * 以j和k作为中心，尝试扩大回文范围
+     *
+     * @param s
+     * @param j
+     * @param k
+     */
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        //(k-1)-(j+1)+1
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
 }
